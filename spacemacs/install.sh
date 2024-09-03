c266fe08a4492954b9d46a3eff96c8d173d7c3ed
Will Simpson <wsimpson@etsy.com>
2024-09-03 10:30:22 -0700

    feat: use Brewfile to install tools

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-brew install ispell ag
-brew tap d12frosted/emacs-plus
-brew install emacs-plus@28 --with-native-comp
 git clone --branch develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
